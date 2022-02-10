044fd9cb2db30a0d4f3f1971716c7cbad3290c8c
vanjantsPIJ <ilya.vanjants@mail.ru>
2022-02-10 11:42:12 +0300

    commit

--- python/lab1/user.py
+++
@@ -1,9 +0,0 @@
-name = input("Как вас зовут?:")
-age = input("Сколько вам лет?:")
-livePlace = input("Где вы живете?:")
-
-age = int(age)
-
-print("Это ", name)
-print("Ему/ей ", age, " лет")
-print("Он/она живет ", livePlace)
